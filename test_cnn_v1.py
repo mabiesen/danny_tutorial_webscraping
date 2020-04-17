e55396a2b71a7c551de0ea2023e18696c8c51dd9
Matthew Biesen <mabiesen@outlook.com>
2020-04-16 19:04:28 -0500

    Update test_cnn_v1.py

--- test_cnn_v1.py
+++ test_cnn_v1.py
@@ -1,19 +1,22 @@
-# this version works, but is spaghetti
+# V1 - working code that is not written well
 
+# these are the external libraries we will be using, we need to import them
+# NOTE: before importing, we had to install them!  this is done with pip, from the terminal.  ex. 'sudo pip install bs4'
 import bs4
 import requests
 
-# get cnn main health page
+# get cnn main health page using the requests library
 # this is fetching the raw html
 res = requests.get('https://www.cnn.com/health')
 res.raise_for_status()
 
-# get all links with in the page using beautifulsoup
+# get all links with in the html using the beautifulsoup library
 # beautifulsoup parses html and makes searching for features easy
 exampleSoup = bs4.BeautifulSoup(res.text, features="html.parser")
 link_elements = exampleSoup.find_all('a', href=True)
 
-# loop all links
+# loop through all the links, add to our link_array 
+# some links already have a prefix, some dont.  
 link_array = []
 for link_element in link_elements:
   if ('www' in link_element['href']) or ('http' in link_element['href']):
@@ -21,7 +24,7 @@ for link_element in link_elements:
   else:
     link_array.append('https://www.cnn.com' + link_element['href'])
 
-# printing all of our links
+# printing all of our links in our link_array
 print('Printing link array:')
 for link in link_array:
   print(link)
